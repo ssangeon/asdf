25aa2ab43b78438690ebc96a42f936eb8b41da44
ssangeon <peleyarsin@naver.com>
2024-04-02 17:12:29 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = 123;
+            string b = "이";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("1");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            A tv01 = new A();
+            A tv02 = new A();
+            tv01.a = 123;
+            tv02.a = 456;
+            Console.WriteLine(tv01.a);
+            Console.WriteLine(tv02.a);
+
+       
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+
+    class A
+    {
+        public int a;
+        public string b = "가";
+
+        
+
+    }
+}
